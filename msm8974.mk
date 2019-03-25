a0ed40066bc72068c34a46abdf7eb51239e8977b
arian <arian.kulmer@web.de>
2019-03-25 20:55:49 +0100

    msm8974-common: Integrate hidl.mk into msm8974.mk

--- msm8974.mk
+++ msm8974.mk
@@ -14,9 +14,6 @@
 
 PLATFORM_PATH := device/sony/msm8974-common
 
-# inherit hidl hals
-$(call inherit-product, $(PLATFORM_PATH)/hidl.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(PLATFORM_PATH)/overlay \
@@ -41,6 +38,10 @@ include $(LOCAL_PATH)/systemprop.mk
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
+PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8974 \
     audio.r_submix.default \
@@ -58,7 +59,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service
+
 # Camera (stock blobs)
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
+
 PRODUCT_PACKAGES += \
     camera.qcom \
     libshims_signal \
@@ -69,6 +78,13 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
+
+PRODUCT_PACKAGES += \
     hwcomposer.msm8974 \
     gralloc.msm8974 \
     memtrack.msm8974 \
@@ -76,6 +92,25 @@ PRODUCT_PACKAGES += \
     libmemalloc \
     liboverlay \
     libqdutils
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
+
+# Dumpstate
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.sony
+
+# FM
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti
 
 # GPS
 PRODUCT_COPY_FILES += \
@@ -91,11 +126,25 @@ PRODUCT_PACKAGES += \
     libloc_ds_api \
     liblocation_api
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
 # Ion
 PRODUCT_PACKAGES += \
     libion
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
 PRODUCT_PACKAGES += \
     lights.msm8974
 
@@ -105,10 +154,27 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
 #  Mobile Data
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
+
+# Netd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0
+
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-service
 
 # Omx
 PRODUCT_PACKAGES += \
@@ -126,20 +192,42 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service-qti
+
 # Recovery
 PRODUCT_PACKAGES += \
     keycheck
+
+# Rendering
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+#RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0-impl \
+    android.hardware.radio.deprecated@1.0-impl
 
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 # Snap Camera
 PRODUCT_PACKAGES += \
     Snap
 
 # Thermal management
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_PACKAGES += \
     thermanager
 
@@ -148,7 +236,23 @@ PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
 
-# Wifi
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
+
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
