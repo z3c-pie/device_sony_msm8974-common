fef2e5c8a327f4d410727e9e0fef9dc3e14cadb6
arian <arian.kulmer@web.de>
2019-03-25 18:58:39 +0100

    msm8974-common: Update BoardConfig
    * commonize some kernel configs
    * drop TARGET_CUSTOM_DTBTOOL := dtbToolLineage it is the default anyway
    * commonize some audio configs
    * drop TARGET_PROVIDES_CAMERA_HAL which is deprecated
    * drop TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS which is deprecated
    * drop TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE which is deprecated
    * drop HEALTHD_ENABLE_TRICOLOR_LED which is deprecated
    * drop BOARD_CHARGER_SHOW_PERCENTAGE which is deprecated
    * drop BOARD_CHARGER_DISABLE_INIT_BLANK which is deprecated
    * drop BACKLIGHT_PATH which is deprecated
    * drop RED_LED_PATH GREEN_LED_PATH BLUE_LED_PATH which are deprecated
    * drop EXTENDED_FONT_FOOTPRINT which is deprecated
    * drop WITH_DEXPREOPT which is set by default
    * drop DEVICE_SPECIFIC_GPS_PATH which is deprecated
    * remove USE_OPENGL_RENDERER

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,28 +14,9 @@
 
 BOARD_VENDOR := sony
 
-# Use Snapdragon LLVM Compiler if available
-TARGET_USE_SDCLANG := true
+PLATFORM_PATH := device/sony/msm8974-common
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH += device/sony/msm8974-common/include
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Kernel properties
-ifeq (,$(filter $(TARGET_KERNEL_SOURCE),))
-  TARGET_KERNEL_SOURCE := kernel/sony/msm8974
-endif
-
-# use CAF variants
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-TARGET_QCOM_MEDIA_VARIANT := caf-msm8974
-
-# Platform
-TARGET_BOOTLOADER_BOARD_NAME := MSM8974
-TARGET_BOARD_PLATFORM := msm8974
+TARGET_SPECIFIC_HEADER_PATH += $(PLATFORM_PATH)/include
 
 # Architecture
 TARGET_ARCH := arm
@@ -43,68 +24,54 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
-TARGET_CUSTOM_DTBTOOL := dtbToolLineage
 
-# Defines for HW subsystems
--include $(PLATFORM_PATH)/hardware/*/BoardConfig.mk
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Kernel
+BOARD_KERNEL_BASE     := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_SOURCE := kernel/sony/msm8974
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
 
 # Audio
-BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_DISABLED_USBAUDIO := true
 AUDIO_FEATURE_ENABLED_EXTN_POST_PROC := true
+BOARD_USES_ALSA_AUDIO := true
+USE_LEGACY_LOCAL_AUDIO_HAL := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
 # Camera
-TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_USES_MEDIA_EXTENSIONS := true
-TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
 
 # Charger
-HEALTHD_ENABLE_TRICOLOR_LED := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BACKLIGHT_PATH :=/sys/class/leds/lcd-backlight/brightness
-RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
-GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
-BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
-
-# Font
-EXTENDED_FONT_FOOTPRINT := true
-
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
 
 # GPS
 USE_DEVICE_SPECIFIC_GPS := true
-DEVICE_SPECIFIC_GPS_PATH := hardware/qcom/gps/sdm845
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm8974
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 TARGET_NO_RPC := true
 
 # Graphics
-USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_USES_GRALLOC1_ADAPTER := true
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2016-05-01
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -123,22 +90,35 @@ BOARD_USES_INIT_SONY := true
 # Lights HAL
 TARGET_PROVIDES_LIBLIGHT := true
 
+# LLVM
+TARGET_USE_SDCLANG := true
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := caf-msm8974
+
 # Power
 TARGET_HAS_LEGACY_POWER_STATS := true
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
 
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+
 # RIL
 TARGET_RIL_VARIANT := caf
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2016-05-01
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
-    device/sony/msm8974-common/sepolicy
-
-# Treble
-DEVICE_MANIFEST_FILE := device/sony/msm8974-common/treble-manifest.xml
+    $(PLATFORM_PATH)/sepolicy
 
 # SODP build barrier
 PRODUCT_PLATFORM_SOD := true
+
+# Treble
+DEVICE_MANIFEST_FILE := device/sony/msm8974-common/treble-manifest.xml
